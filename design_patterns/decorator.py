7e9aa20464ce8fead52921b1cbbe1bd9fbaf96ca
ASVARYCH <asvarych@luxoft.com>
2023-02-10 19:58:10 +0200

    removed english description from decorator.py

--- design_patterns/decorator.py
+++ design_patterns/decorator.py
@@ -11,7 +11,7 @@ class BoldWrapper(TextTag):
         self._wrapped = wrapped
 
     def render(self):
-        return "<b>{}</b>".format(self._wrapped.render())
+        return f"<b>{self._wrapped.render()}</b>"
 
 
 class ItalicWrapper(TextTag):
@@ -19,7 +19,7 @@ class ItalicWrapper(TextTag):
         self._wrapped = wrapped
 
     def render(self):
-        return "<i>{}</i>".format(self._wrapped.render())
+        return f"<i>{self._wrapped.render()}</i>"
 
 
 if __name__ == "__main__":
@@ -27,17 +27,6 @@ if __name__ == "__main__":
     special_hello = ItalicWrapper(BoldWrapper(simple_hello))
     print(simple_hello.render())
     print(special_hello.render())
-
-
-# In this example, the TextTag class is a simple implementation of a text tag
-# that holds a string of text. The BoldWrapper and ItalicWrapper classes
-# are decorator classes that wrap instances of TextTag and add HTML
-# bold and italic tags to the rendered text, respectively.
-# The if __name__ == "__main__" block is the entry point of the program.
-# When the program is run, it creates a TextTag instance with
-# the text "Hello, world!", and then wraps it in BoldWrapper
-# and ItalicWrapper instances. The render method of the resulting special_hello
-# instance is then called to produce the final HTML output.
 
 
 # У цьому прикладі клас TextTag є простою реалізацією текстового тегу,
