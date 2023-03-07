28d8edc8d08349fe26cd0c9e7f3ed28546a65d2f
ASVARYCH <asvarych@luxoft.com>
2023-03-07 19:47:15 +0200

    chrome_options renamed to options

--- conftest.py
+++ conftest.py
@@ -6,16 +6,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope='package', name='demoqa', autouse=True)
 def set_browser_for_demoqa():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.headless = True
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument('--start-maximized')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument("--disable-setuid-sandbox")
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    options.add_argument('--start-maximized')
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-setuid-sandbox")
     driver = webdriver.Chrome(
         executable_path=ChromeDriverManager().install(),
-        options=chrome_options)
+        options=options)
     config.base_url = 'https://demoqa.com/'
     config.driver = driver
     yield driver
