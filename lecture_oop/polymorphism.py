bd3dcfdfb42dcdcba78ea7c33f10bfc2dd1a7c82
ASVARYCH <asvarych@luxoft.com>
2023-03-28 00:42:26 +0300

    polymophism examples

--- lecture_oop/polymorphism.py
+++ lecture_oop/polymorphism.py
@@ -1,53 +1,14 @@
-###### Polymorphism of methods
 import math
+# ####### Polymorphism of operators
+# print(5 + 6)  # prints: 11
+# print('Hello, ' + 'world!')  # prints: Hello, world!
 
+# ######## Polymorphism of functions
+# print(len('Hello'))  # prints: 5
+# print(len([111, 222, 33]))  # prints: 3
 
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return math.pi * self.radius ** 2
-
-class Square:
-    def __init__(self, side):
-        self.side = side
-
-    def area(self):
-        return self.side ** 2
-
-shapes = [Circle(5), Square(10)]
-for shape in shapes:
-    print(shape.area())
-
-####### Polymorphism of operators
-print(5 + 6)  # prints: 11
-print('Hello, ' + 'world!')  # prints: Hello, world!
-
-######## Polymorphism of functions
-print(len('Hello'))  # prints: 5
-print(len([1, 2, 3]))  # prints: 3
-
-####### Polymorphism through inheritance
-class Animal:
-    def speak(self):
-        pass
-
-class Dog(Animal):
-    def speak(self):
-        return 'Woof!'
-
-class Cat(Animal):
-    def speak(self):
-        return 'Meow!'
-
-animals = [Dog(), Cat()]
-for animal in animals:
-    print(animal.speak())
-
-
-'''
-Поліморфізм через успадкування полягає в тому, що класи можуть успадковувати 
+"""
+Поліморфізм через успадкування полягає в тому, що класи можуть успадковувати
 властивості і методи від батьківського класу і використовувати їх у своїй 
 реалізації. Якщо клас успадковує метод з батьківського класу, то він може 
 перевизначити його власною реалізацією, яка буде викликатися замість 
@@ -59,4 +20,95 @@ for animal in animals:
 Таким чином, ми можемо використовувати ці класи з методом "говорити" в одному 
 і тому ж контексті, але отримувати різні результати, що і є суть поліморфізму 
 через успадкування.
-'''
+"""
+
+
+class Human:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print(f'Hey, I am human {self.name}')
+
+
+class Employee(Human):
+    def greet(self):
+        print(f'Hey, I am employee {self.name}')
+
+
+class Programmer(Human):
+    def greet(self):
+        print(f'Hey, I am programmer {self.name}')
+
+
+human = Human('Vanya')
+employee = Employee('Andrew')
+programmer = Programmer('Artem')
+
+humans = [human, employee, programmer]
+for person in humans:
+    assert isinstance(person, Human)
+    person.greet()
+
+
+class Shape:
+    def __init__(self, **kwargs):
+        self.radius = kwargs.get('radius')
+        self.sideA = kwargs.get('sideA')
+        self.sideB = kwargs.get('sideB')
+        self.sideC = kwargs.get('sideC')
+
+    def area(self):
+        pass
+
+
+class Circle(Shape):
+    """
+    :param radius
+    :type int or float
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+
+class Square(Shape):
+    """
+    :param sideA or sideB or sideC: will be used first one --side of square
+    :type int or float
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__side = self.sideA or self.sideB or self.sideC
+
+    def area(self):
+        return self.__side ** 2
+
+
+class Rectangle(Shape):
+    """
+    :param sideA -- height
+    :param sideB -- width
+    :type int or float
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__sideA = self.sideA
+        self.__sideB = self.sideB
+
+    def area(self):
+        return self.__sideA * self.__sideB
+
+
+circle = Circle(radius=7)
+square = Square(sideA=5)
+rectangle = Rectangle(sideA=5, sideB=6)
+
+for shape in [circle, square, rectangle]:
+    assert isinstance(shape, Shape)
+    print(shape.area())
