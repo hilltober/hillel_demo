31f34f5290ffdb0e2636962a2b5bb3f242d9c344
ASVARYCH <asvarych@luxoft.com>
2023-04-07 18:10:43 +0300

    added implicitly wait to fixture 'chrome'

--- lecture_selenium/conftest.py
+++ lecture_selenium/conftest.py
@@ -1,9 +1,11 @@
 import pytest
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 @pytest.fixture(scope='class')
 def chrome(request):
-    driver = request.getfixturevalue("demoqa")
+    driver: WebDriver = request.getfixturevalue("demoqa")
+    driver.implicitly_wait(4)
     if request.cls:
         request.cls.driver = driver
     yield driver
